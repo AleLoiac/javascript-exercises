3336e9001ef0ce6f6f1784dfd63069e4cf42527b
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-03 19:11:50 +0200

    Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    
+    let smallerInt = 0;
+    let biggerInt = 0;
+    if (a < b) {
+        smallerInt = a;
+        biggerInt = b;
+    } else {
+        smallerInt = b;
+        biggerInt = a;
+    }
 
+    let sum = 0;
+    for (let i = smallerInt; i <= biggerInt ; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
