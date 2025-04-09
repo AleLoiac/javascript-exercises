08664b42e5c459d24da5cd1ad48ced5afafcf8ad
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-09 11:46:34 +0200

    Implement palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    regex = /[\s\.,?!]/g
+    
+    return str
+        .toLowerCase()
+        .replace(regex, "")
+        .split("")
+        .reverse()
+        .join("")
+        === str
+        .toLowerCase()
+        .replace(regex, "")
+        .split("")
+        .join("");
 };
 
 // Do not edit below this line
