e7f0f77b8302367e619406a9186b99ec051d8d79
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-09 12:15:02 +0200

    Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
 
+    const fibonacci = [0, 1];
+
+    for (let i = 2; i < position + 1; i ++) {
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+
+    return fibonacci[position];
 };
 
 // Do not edit below this line
