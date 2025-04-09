075ad52dd207958b10635641621496474b5648ae
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-09 14:38:13 +0200

    Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.forEach(element => {
+        if (!element.yearOfDeath) {
+            element.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
+    console.log(arr);
+    
+    arr.sort((personA, personB) => (personB.yearOfDeath-personB.yearOfBirth) - (personA.yearOfDeath-personA.yearOfBirth));
+    return arr[0];
 };
 
 // Do not edit below this line
